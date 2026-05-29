75a0a631645f4f35ea1f61704b26f42648e1b8e7
Gleizits <gleizits@protonmail.com>
2026-05-28 20:16:18 -0500

    Productos Ordenados por Ciudad que sean de USA, se utilizaron windows functions

--- TSQL Scripts/products_city_quantity.sql
+++ TSQL Scripts/products_city_quantity.sql
@@ -1,10 +1,11 @@
 USE Northwind
 GO
 
-SELECT p.ProductName, c.City, od.Quantity
+SELECT p.ProductName, c.City, od.Quantity,
+DENSE_RANK () OVER (PARTITION BY c.City ORDER BY od.Quantity DESC) AS RANK 
 FROM [Customers] c
 INNER JOIN [Orders] o ON c.CustomerID = o.CustomerID
 INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
 INNER JOIN [Products] p ON od.ProductID = p.ProductID
 WHERE c.Country = 'USA'
-ORDER BY c.City, od.Quantity ASC;
+ORDER BY od.Quantity DESC;
